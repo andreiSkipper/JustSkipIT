fbe9ebca88196cc319367b0be8470c074c783c93
andrei <andrei@boostit.net>
2017-09-01 14:30:29 +0300

    social facebook extension

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,17 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/messages', // if advanced application, set @frontend/messages
+                    'sourceLanguage' => 'en',
+                    'fileMap' => [
+                        //'main' => 'main.php',
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -70,4 +70,45 @@ return [
         ],
     ],
     'params' => $params,
+    'modules' => [
+        'social' => [
+            // the module class
+            'class' => 'kartik\social\Module',
+
+            // the global settings for the Disqus widget
+            'disqus' => [
+                'settings' => ['shortname' => 'DISQUS_SHORTNAME'] // default settings
+            ],
+
+            // the global settings for the Facebook plugins widget
+            'facebook' => [
+                'appId' => '167738380465356',
+                'secret' => '3a78e2e24a99339e837cb3c6ffed5547',
+            ],
+
+            // the global settings for the Google+ Plugins widget
+            'google' => [
+                'clientId' => 'GOOGLE_API_CLIENT_ID',
+                'pageId' => 'GOOGLE_PLUS_PAGE_ID',
+                'profileId' => 'GOOGLE_PLUS_PROFILE_ID',
+            ],
+
+            // the global settings for the Google Analytics plugin widget
+            'googleAnalytics' => [
+                'id' => 'TRACKING_ID',
+                'domain' => 'TRACKING_DOMAIN',
+            ],
+
+            // the global settings for the Twitter plugin widget
+            'twitter' => [
+                'screenName' => 'darkbanel'
+            ],
+
+            // the global settings for the GitHub plugin widget
+            'github' => [
+                'settings' => ['user' => 'GITHUB_USER', 'repo' => 'GITHUB_REPO']
+            ],
+        ],
+        // your other modules
+    ]
 ];
