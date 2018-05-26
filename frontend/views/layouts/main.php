e5dfb181f5484557bbadf57127f57574f43f35ef
root <root@ip-172-31-32-166.eu-central-1.compute.internal>
2018-05-26 17:37:42 +0300

    added bucharest timezone

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -38,8 +38,8 @@ $this->title = "JustSkipIT | " . $this->title;
 
     <script src='/js/socket.io.js'></script>
     <script>
-        // var socket = io('http://ec2-35-159-26-29.eu-central-1.compute.amazonaws.com:3002');
-        var socket = io('http://localhost:3002');
+        var socket = io('http://ec2-35-159-26-29.eu-central-1.compute.amazonaws.com:3002');
+        // var socket = io('http://localhost:3002');
         //$(document).ready(function () {
         //    var model_user = <?//= json_encode(\common\models\User::find()->where(['id' => Yii::$app->user->identity->id])->one()->toArray()) ?>//;
         //    var model_profile = <?//= json_encode(\common\models\Profiles::find()->where(['user_id' => Yii::$app->user->identity->id])->one()->toArray()) ?>//;
